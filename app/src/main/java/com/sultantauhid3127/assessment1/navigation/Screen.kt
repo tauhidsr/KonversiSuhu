c6e75bdc6e3389bf5cabe06121b3a7a3eda87660
tauhidsr <sultan.tauhidd@gmail.com>
2025-04-11 00:13:39 +0700

    membuat class Screen

--- app/src/main/java/com/sultantauhid3127/assessment1/navigation/Screen.kt
+++ app/src/main/java/com/sultantauhid3127/assessment1/navigation/Screen.kt
@@ -1,2 +1,7 @@
 package com.sultantauhid3127.assessment1.navigation
 
+sealed class Screen(val route: String) {
+    data object Home : Screen("home")
+    data object Convert : Screen("convert")
+    data object About : Screen("about")
+}
