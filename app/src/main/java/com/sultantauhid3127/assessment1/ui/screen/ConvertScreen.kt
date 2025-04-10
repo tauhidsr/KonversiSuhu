a1e46df7b8eeef937e806d7b5be8691a7854085d
tauhidsr <sultan.tauhidd@gmail.com>
2025-04-11 02:09:01 +0700

    memindahkan teks ke string untuk multi-bahasa

--- app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/ConvertScreen.kt
+++ app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/ConvertScreen.kt
@@ -1,5 +1,6 @@
 package com.sultantauhid3127.assessment1.ui.screen
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -7,7 +8,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.sultantauhid3127.assessment1.ui.theme.Assessment1Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,6 +28,17 @@ fun ConvertScreen(navController: NavController) {
         Text(
             text = "Ini Convert Screen",
             modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ConvertScreenPreview() {
+    Assessment1Theme {
+        ConvertScreen(
+            navController = rememberNavController()
         )
     }
 }
--- app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/HomeScreen.kt
@@ -1,29 +1,84 @@
 package com.sultantauhid3127.assessment1.ui.screen
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.sultantauhid3127.assessment1.R
+import com.sultantauhid3127.assessment1.navigation.Screen
+import com.sultantauhid3127.assessment1.ui.theme.Assessment1Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController, onChangeTheme: () -> Unit) {
+    var showMenu by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
-                    Text("Konversi Suhu")
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                actions = {
+                    IconButton(onClick = { showMenu = !showMenu}) {
+                        Icon(Icons.Default.MoreVert, contentDescription = stringResource(id = R.string.menu))
+                    }
+                    DropdownMenu(
+                        expanded = showMenu,
+                        onDismissRequest = {showMenu = false}
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text(stringResource(id = R.string.menu_about))},
+                            onClick = {
+                                showMenu = false
+                                navController.navigate(Screen.About.route)
+                            }
+                        )
+                        DropdownMenuItem(
+                            text = { Text(stringResource(id = R.string.menu_theme))},
+                            onClick = {
+                                showMenu = false
+                                onChangeTheme()
+                            }
+                        )
+                    }
                 }
             )
         }
     ) { innerPadding ->
         Text(
-            text = "Ini Home Screen",
+            text = stringResource(id = R.string.home_screen_text),
             modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    Assessment1Theme {
+        HomeScreen(
+            navController = rememberNavController(),
+            onChangeTheme = {}
         )
     }
 }
