2c2584e78de7ef21fb5af8f6f63696345b130092
tauhidsr <sultan.tauhidd@gmail.com>
2025-04-11 05:57:15 +0700

    menambahkan input suhu dan dropdown jenis suhu

--- app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/ConvertScreen.kt
+++ app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/ConvertScreen.kt
@@ -1,19 +1,27 @@
 package com.sultantauhid3127.assessment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.sultantauhid3127.assessment1.R
@@ -22,6 +30,11 @@ import com.sultantauhid3127.assessment1.ui.theme.Assessment1Theme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ConvertScreen(navController: NavController) {
+    var suhuInput by rememberSaveable { mutableStateOf("") }
+    val jenisSuhuList = listOf("Celsius", "Fahrenheit", "Kelvin")
+    var expanded by remember { mutableStateOf(false) }
+    var selectedSuhu by rememberSaveable { mutableStateOf(jenisSuhuList[0]) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -31,7 +44,7 @@ fun ConvertScreen(navController: NavController) {
                 navigationIcon = {
                     IconButton(onClick = {navController.popBackStack()}) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = stringResource(R.string.back)
                         )
                     }
@@ -39,10 +52,57 @@ fun ConvertScreen(navController: NavController) {
             )
         }
     ) { innerPadding ->
-        Text(
-            text = stringResource(id = R.string.convert_screen_text),
-            modifier = Modifier.padding(innerPadding)
-        )
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = suhuInput,
+                onValueChange = { suhuInput = it},
+                label = { Text(stringResource(id = R.string.label_input_suhu))},
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = {expanded = !expanded}
+            ) {
+                OutlinedTextField(
+                    value = selectedSuhu,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = { Text(stringResource(id = R.string.label_input_suhu))},
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                    },
+                    modifier = Modifier
+                        .menuAnchor(MenuAnchorType.PrimaryNotEditable, enabled = true)
+                        .fillMaxWidth()
+                )
+
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = {expanded = false}
+                ) {
+                    jenisSuhuList.forEach { suhu ->
+                        DropdownMenuItem(
+                            text = { Text(suhu)},
+                            onClick = {
+                                selectedSuhu = suhu
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+
+            }
+        }
     }
 }
 
