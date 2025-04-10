22a02e82832fec389daa7b7b0235207e83039ae9
tauhidsr <sultan.tauhidd@gmail.com>
2025-04-11 03:02:43 +0700

    menambahkan logo di HomeScreen

--- app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/HomeScreen.kt
@@ -1,7 +1,10 @@
 package com.sultantauhid3127.assessment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
@@ -17,9 +20,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.sultantauhid3127.assessment1.R
@@ -64,10 +70,24 @@ fun HomeScreen(navController: NavController, onChangeTheme: () -> Unit) {
             )
         }
     ) { innerPadding ->
-        Text(
-            text = stringResource(id = R.string.home_screen_text),
-            modifier = Modifier.padding(innerPadding)
-        )
+
+        androidx.compose.foundation.layout.Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = stringResource(R.string.app_name),
+                modifier = Modifier
+                    .size(120.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.home_screen_text),
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
     }
 }
 
