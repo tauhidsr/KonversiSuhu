b95a7ef3fba74e5c81f0124c72640a747f02f693
tauhidsr <sultan.tauhidd@gmail.com>
2025-04-11 17:28:15 +0700

    membuat isi konten di AboutScreen dan desain top app barnya

--- app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/AboutScreen.kt
+++ app/src/main/java/com/sultantauhid3127/assessment1/ui/screen/AboutScreen.kt
@@ -7,13 +7,16 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.sultantauhid3127.assessment1.R
@@ -26,22 +29,32 @@ fun AboutScreen(navController: NavController) {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = stringResource(R.string.app_name))
+                    Text(
+                        text = stringResource(R.string.about),
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
                 },
                 navigationIcon = {
-                    IconButton(onClick = {navController.popBackStack()}) {
+                    IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = stringResource(R.string.back)
+                            contentDescription = stringResource(R.string.back),
+                            tint = MaterialTheme.colorScheme.onPrimary
                         )
                     }
-                }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
             )
         }
     ) { innerPadding ->
         Text(
-            text = stringResource(id = R.string.about_screen_text),
-            modifier = Modifier.padding(innerPadding)
+            text = stringResource(id = R.string.about_description),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
         )
     }
 }
